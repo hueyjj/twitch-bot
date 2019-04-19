c7ce9ba8fefc4a9f871264cd965e6cc3616d1920
hueyjj <jasperjeng@gmail.com>
2019-04-18 18:33:37 -0700

    Check if BOT_USERNAME, CHANNEL_NAME, OAUTH_TOKEN are set

--- main.go
+++ main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
-	"fmt"
-	"github.com/hueyjj/twitch-bot/irc"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
 )
 
 func main() {
-	fmt.Printf("hello")
-	irc.PrintIrc()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	botUsername := os.Getenv("BOT_USERNAME")
+	channelName := os.Getenv("CHANNEL_NAME")
+	oAuthToken := os.Getenv("OAUTH_TOKEN")
+
+	if botUsername == "" || channelName == "" || oAuthToken == "" {
+		log.Fatalf("Environmental variables not set: BOT_USERNAME=%s CHANNEL_NAME=%s OAUTH_TOKEN=%s\n", botUsername, channelName, oAuthToken)
+	} else {
+		log.Printf("Environmental variables set: BOT_USERNAME=%s CHANNEL_NAME=%s OAUTH_TOKEN=%s\n", botUsername, channelName, oAuthToken)
+	}
 }
